507c64a0d2811718527b3389383a6164d7826865
ewaugh13 <ewaugh13@yahoo.com>
2019-08-28 00:10:28 -0600

    Have monster creation and deletion occuring. Fixed bug with char* changing after being set in helper method by making it a reference pointer. Not sure if there is a better way but it works.

--- MonsterChase/MonsterChase/MonsterChase.cpp
+++ MonsterChase/MonsterChase/MonsterChase.cpp
@@ -16,17 +16,16 @@ const int GRID_SIZE = 100;
 // number of turns that go by before we generate a new monster
 const int TURN_AMOUNT_GENERATE_MONSTER = 9;
 
-void getPlayerInput(char * input);
+void getPlayerInput(char * &input);
 void setCharacterLocation(Character * character);
-void gamePlayLoop(Character * player, Character * monsters, int& numMonsters);
+void gamePlayLoop(Character * player, Character * &monsters, int& numMonsters);
 void monsterAI(Character * monsters, int& numMonsters);
 
 int main()
 {
 	// get the number of monsters to create
 	std::cout << "How many monsters do you want to start with: ";
-	char * numMonstersInput = (char *)calloc(1, sizeof(char));
-
+	char * numMonstersInput = (char *)calloc(1, sizeof(char)); 
 	getPlayerInput(numMonstersInput);
 	int numMonsters = atoi(numMonstersInput);
 
@@ -37,7 +36,7 @@ int main()
 	for (int i = 0; i < numMonsters; i++)
 	{
 		std::cout << "What would you like to name Monster " << i << ": ";
-		monsters[i].name = (char*)calloc(1, sizeof(char*));
+		monsters[i].name = (char *)calloc(1, sizeof(char));
 		getPlayerInput(monsters[i].name);
 
 		setCharacterLocation(&monsters[i]);
@@ -45,7 +44,7 @@ int main()
 
 	// get the player name and set a location
 	std::cout << "What would you like to name the Player" << ": ";
-	char * playerName = (char *)calloc(1, sizeof(char));
+	char * playerName = (char *)calloc(1, sizeof(char)); 
 	getPlayerInput(playerName);
 	player->name = playerName;
 	player->x, player->y = 0;
@@ -55,19 +54,23 @@ int main()
 
 	// free everything we allocated
 	free(numMonstersInput);
+	for (int i = 0; i < numMonsters; i++)
+	{
+		free(monsters[i].name);
+	}
 	free(monsters);
 	free(player);
 	free(playerName);
 }
 
 // read player input 1 char at a time until enter a cariage return is read
-void getPlayerInput(char * input)
+void getPlayerInput(char * &input)
 {
-	char charelement;
+	char charElement;
 	int i = 0;
-	while ((charelement = (char)_getche()) != '\r')
+	while ((charElement = (char)_getche()) != '\r')
 	{
-		input[i] = charelement;
+		input[i] = charElement;
 		// i + 2 to store new element and possible end delimiter
 		input = (char *)realloc((void *)input, (i + 2) * sizeof(char));
 
@@ -90,7 +93,7 @@ void setCharacterLocation(Character * character)
 }
 
 // does the main gameplay loop
-void gamePlayLoop(Character * player, Character * monsters, int& numMonsters)
+void gamePlayLoop(Character * player, Character * &monsters, int& numMonsters)
 {
 	bool playing = true;
 	int currentTurnCounter = 0;
@@ -100,16 +103,27 @@ void gamePlayLoop(Character * player, Character * monsters, int& numMonsters)
 		{
 			currentTurnCounter = 0;
 			numMonsters++;
-			//TODO: create monster
+
+			monsters = (Character *)realloc(monsters, numMonsters * sizeof(Character));
+
+			int newMemSizeMonsters = _msize(monsters);
+			newMemSizeMonsters;
+			// create new monster
+			monsters[numMonsters - 1].name = (char *)calloc(2, sizeof(char));
+			monsters[numMonsters - 1].name[0] = (char)(rand() % 255);
+			monsters[numMonsters - 1].name[1] = '\0';
+			setCharacterLocation(&monsters[numMonsters - 1]);
 		}
 
 		// print monster locations
 		for (int i = 0; i < numMonsters; i++)
 		{
-			printf("Monster %s at [%02d,%02d]\n", monsters[i].name, monsters[i].x, monsters[i].y);
+			std::cout << "Monster " << monsters[i].name;
+			printf(" at [%02d,%02d]\n", monsters[i].x, monsters[i].y);
 		}
 		// print player location
-		printf("Player %s at [%02d,%02d]\n", player->name, player->x, player->y);
+		std::cout << "Player " << player->name;
+		printf(" at [%02d,%02d]\n", player->x, player->y);
 
 		// print instructions for player
 		std::cout << "Press A to move left, D to move right, W to move up, S to move down or Q to quit." << std::endl;
@@ -156,6 +170,12 @@ void gamePlayLoop(Character * player, Character * monsters, int& numMonsters)
 			if (player->x == monsters[i].x && player->y == monsters[i].y)
 			{
 				// TODO: destroy monster
+				numMonsters--;
+				Character * monstersTemp = (Character *)calloc(numMonsters, sizeof(Character));
+				memcpy((void*)monstersTemp, (void*)monsters, sizeof(Character) * i - 1);
+				memcpy(&monstersTemp[i], &monsters[i + 1], sizeof(Character) * (numMonsters - i));
+				free(monsters);
+				monsters = monstersTemp;
 			}
 		}
 		currentTurnCounter++;
