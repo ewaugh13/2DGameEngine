df9fa2cd47fb57404f1ecac840da55341fe13554
ewaugh13 <ewaugh13@yahoo.com>
2019-11-01 20:30:16 -0600

    Inlined Point2D and added regions for organization

---
+++ MonsterChase/Engine/Point2D-inl.h
@@ -0,0 +1,212 @@
+#include "Point2D.h"
+#pragma once
+
+#pragma region Constructor and Destructor
+inline Point2D::Point2D(const Point2D & i_rhs)
+{
+	this->m_x = i_rhs.m_x;
+	this->m_y = i_rhs.m_y;
+}
+
+inline Point2D::~Point2D()
+{
+}
+#pragma endregion
+
+#pragma region Incrementers and Decrementers
+inline void Point2D::incrementXValue()
+{
+	this->m_x++;
+}
+
+inline void Point2D::decrementXValue()
+{
+	this->m_x--;
+}
+
+inline void Point2D::incrementYValue()
+{
+	this->m_y++;
+}
+
+inline void Point2D::decrementYValue()
+{
+	this->m_y--;
+}
+#pragma endregion
+
+#pragma region Class Operator Overloads
+inline Point2D& Point2D::operator=(const Point2D & i_rhs)
+{
+	this->m_x = i_rhs.m_x;
+	this->m_y = i_rhs.m_y;
+	return *this;
+}
+
+#pragma region Arithmetic Operators
+inline Point2D& Point2D::operator-() const
+{
+	Point2D newPoint = Point2D(-this->m_x, -this->m_y);
+	return newPoint;
+}
+
+inline Point2D& Point2D::operator*(float scalar) const
+{
+	Point2D newPoint = Point2D(this->m_x * scalar, this->m_y * scalar);
+	return newPoint;
+}
+
+inline Point2D& Point2D::operator/(float scalar) const
+{
+	Point2D newPoint = Point2D(this->m_x / scalar, this->m_y / scalar);
+	return newPoint;
+}
+
+inline void Point2D::operator++()
+{
+	++this->m_x, ++this->m_y;
+}
+
+inline void Point2D::operator++(int)
+{
+	this->m_x++, this->m_y++;
+}
+
+inline void Point2D::operator--()
+{
+	--this->m_x, --this->m_y;
+}
+
+inline void Point2D::operator--(int)
+{
+	this->m_x--, this->m_y--;
+}
+#pragma endregion
+
+#pragma region Compound Assignment Operators
+inline Point2D& Point2D::operator+=(const Point2D & point)
+{
+	this->m_x += point.m_x;
+	this->m_y += point.m_y;
+	return *this;
+}
+
+inline Point2D& Point2D::operator-=(const Point2D & point)
+{
+	this->m_x -= point.m_x;
+	this->m_y -= point.m_y;
+	return *this;
+}
+
+inline Point2D& Point2D::operator*=(const Point2D & point)
+{
+	this->m_x *= point.m_x;
+	this->m_y *= point.m_y;
+	return *this;
+}
+
+inline Point2D& Point2D::operator*=(float scalar)
+{
+	this->m_x *= scalar;
+	this->m_y *= scalar;
+	return *this;
+}
+
+inline Point2D& Point2D::operator/=(const Point2D & point)
+{
+	this->m_x /= point.m_x;
+	this->m_y /= point.m_y;
+	return *this;
+}
+
+inline Point2D& Point2D::operator/=(float scalar)
+{
+	this->m_x /= scalar;
+	this->m_y /= scalar;
+	return *this;
+}
+#pragma endregion
+
+#pragma endregion
+
+
+#pragma region Non Class Methods
+
+#pragma region Point2D Math
+inline float dot(const Point2D & i_lhs, const Point2D & i_rhs)
+{
+	return i_lhs.X() * i_rhs.X() + i_lhs.Y() * i_rhs.Y();
+}
+
+inline float cross(const Point2D & i_lhs, const Point2D & i_rhs)
+{
+	return i_lhs.X() * i_rhs.Y() - i_lhs.Y() * i_rhs.X();
+}
+#pragma endregion
+
+#pragma region Non-Class Operator Overloads
+
+#pragma region Arithmetic Operators
+inline Point2D operator+(const Point2D & leftPoint, const Point2D & rightPoint)
+{
+	return Point2D(leftPoint.X() + rightPoint.X(), leftPoint.Y() + rightPoint.Y());
+}
+
+inline Point2D operator-(const Point2D & leftPoint, const Point2D & rightPoint)
+{
+	return Point2D(leftPoint.X() - rightPoint.X(), leftPoint.Y() - rightPoint.Y());
+}
+
+inline Point2D operator*(const Point2D & leftPoint, const Point2D & rightPoint)
+{
+	return Point2D(leftPoint.X() * rightPoint.X(), leftPoint.Y() * rightPoint.Y());
+}
+
+inline Point2D operator/(const Point2D & leftPoint, const Point2D & rightPoint)
+{
+	return Point2D(leftPoint.X() / rightPoint.X(), leftPoint.Y() / rightPoint.Y());
+}
+#pragma endregion
+
+#pragma region Comparision Operators
+inline bool operator==(const Point2D & leftPoint, const Point2D & rightPoint)
+{
+	return leftPoint.X() == rightPoint.X() && leftPoint.Y() == rightPoint.Y();
+}
+inline bool operator!=(const Point2D & leftPoint, const Point2D & rightPoint)
+{
+	return leftPoint.X() != rightPoint.X() || leftPoint.Y() != rightPoint.Y();
+}
+
+inline bool operator>(const Point2D & leftPoint, const Point2D & rightPoint)
+{
+	double currentSize = sqrt(pow(leftPoint.X(), 2) + pow(leftPoint.Y(), 2));
+	double pointSize = sqrt(pow(rightPoint.X(), 2) + pow(rightPoint.Y(), 2));
+	return currentSize > pointSize;
+}
+
+inline bool operator<(const Point2D & leftPoint, const Point2D & rightPoint)
+{
+	double currentSize = sqrt(pow(leftPoint.X(), 2) + pow(leftPoint.Y(), 2));
+	double pointSize = sqrt(pow(rightPoint.X(), 2) + pow(rightPoint.Y(), 2));
+	return currentSize < pointSize;
+}
+
+inline bool operator>=(const Point2D & leftPoint, const Point2D & rightPoint)
+{
+	double currentSize = sqrt(pow(leftPoint.X(), 2) + pow(leftPoint.Y(), 2));
+	double pointSize = sqrt(pow(rightPoint.X(), 2) + pow(rightPoint.Y(), 2));
+	return currentSize >= pointSize;
+}
+
+inline bool operator<=(const Point2D & leftPoint, const Point2D & rightPoint)
+{
+	double currentSize = sqrt(pow(leftPoint.X(), 2) + pow(leftPoint.Y(), 2));
+	double pointSize = sqrt(pow(rightPoint.X(), 2) + pow(rightPoint.Y(), 2));
+	return currentSize <= pointSize;
+}
+#pragma endregion
+
+#pragma endregion
+
+#pragma endregion
--- MonsterChase/Engine/Point2D.h
+++ MonsterChase/Engine/Point2D.h
@@ -5,32 +5,54 @@
 class Point2D
 {
 public:
-	Point2D(float x, float y);
-	~Point2D();
+#pragma region Constructor and Destructor
+	// standard constructor
+	Point2D(float i_x, float i_y) :
+		m_x(i_x),
+		m_y(i_y)
+	{
+	}
 
+	// copy constructor
+	Point2D(const Point2D & i_rhs);
+
+	// standard destructor
+	~Point2D();
+#pragma endregion
+
+#pragma region Getters and Setters
 	// Getters for private variables
-	float X() const { return m_x; }
-	float Y() const { return m_y; }
+	inline float X() const { return m_x; }
+	inline float Y() const { return m_y; }
 
 	// Setters for private variables
-	void X(float i_x) { m_x = i_x; }
-	void Y(float i_y) { m_y = i_y; }
+	inline void X(float i_x) { m_x = i_x; }
+	inline void Y(float i_y) { m_y = i_y; }
+#pragma endregion
 
-	// incrementers and decrementers for x and y
+#pragma region Incrementers and Decrementers
 	void incrementXValue();
 	void decrementXValue();
 	void incrementYValue();
 	void decrementYValue();
+#pragma endregion
 
-	// operator overloads
+#pragma region Const Point2D
+	// const point2D {0.0f, 0.0f, 0.0f}
+	static const Point2D Zero;
+#pragma endregion
 
-	// arithmetic operators
+#pragma region Class Operator Overloads
+	// assignment operator
+	Point2D& operator=(const Point2D & i_rhs);
+
+#pragma region Arithmetic Operators
 	// negate the x and y values of the current point
-	Point2D operator-() const;
+	Point2D& operator-() const;
 	// multiply scalar to current point and return new value
-	Point2D operator*(float scalar) const;
+	Point2D& operator*(float scalar) const;
 	// divide current by scalar and return new value
-	Point2D operator/(float scalar) const;
+	Point2D& operator/(float scalar) const;
 	// prefix adition to current
 	void operator++();
 	// postfix adition to current
@@ -39,37 +61,28 @@ public:
 	void operator--();
 	// postfix subtraction to current
 	void operator--(int);
+#pragma endregion
 
-	// compound assignment operators
+#pragma region Compound Assignment Operators
 	// add point value to current point
-	Point2D& operator+=(const Point2D &point);
+	Point2D& operator+=(const Point2D & point);
 	// subtract point value from current point
-	Point2D& operator-=(const Point2D &point);
+	Point2D& operator-=(const Point2D & point);
 	// multiply point value and current point
-	Point2D& operator*=(const Point2D &point);
+	Point2D& operator*=(const Point2D & point);
 	// multiply current value by a scalar
 	Point2D& operator*=(float scalar);
 	// divide current point by point
-	Point2D& operator/=(const Point2D &point);
+	Point2D& operator/=(const Point2D & point);
 	// divide current point by scalar
 	Point2D& operator/=(float scalar);
+#pragma endregion
+
+#pragma endregion
+
 
 private:
 	float m_x, m_y;
 };
 
-// non class operator overloads
-
-// arithmetic operators
-Point2D operator+(const Point2D &leftPoint, const Point2D &rightPoint);
-Point2D operator-(const Point2D &leftPoint, const Point2D &rightPoint);
-Point2D operator*(const Point2D &leftPoint, const Point2D &rightPoint);
-Point2D operator/(const Point2D &leftPoint, const Point2D &rightPoint);
-
-// comparision operators
-bool operator==(const Point2D &leftPoint, const Point2D &rightPoint);
-bool operator!=(const Point2D &leftPoint, const Point2D &rightPoint);
-bool operator>(const Point2D &leftPoint, const Point2D &rightPoint);
-bool operator<(const Point2D &leftPoint, const Point2D &rightPoint);
-bool operator>=(const Point2D &leftPoint, const Point2D &rightPoint);
-bool operator<=(const Point2D &leftPoint, const Point2D &rightPoint);
+#include "Point2D-inl.h"
