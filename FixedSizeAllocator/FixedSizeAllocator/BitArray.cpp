ff1d27ad7bd84ff2e75081690a369f27baa39a14
ewaugh13 <ewaugh13@yahoo.com>
2019-12-12 01:26:14 -0700

    Fleshed out BitArray methods. Going create a unit test to verify they are working

--- FixedSizeAllocator/FixedSizeAllocator/BitArray.cpp
+++ FixedSizeAllocator/FixedSizeAllocator/BitArray.cpp
@@ -1,7 +1,9 @@
 #include "BitArray.h"
 
 BitArray::BitArray(size_t i_numBits, HeapManager * i_pAllocator, bool i_startClear = true) :
-	m_numBits(i_numBits), m_numBytes(i_numBits / 8)
+	m_allocatedUsed(i_pAllocator),
+	m_numBits(i_numBits),
+	m_numBytes(i_numBits / 8)
 {
 	m_pBits = reinterpret_cast<uint64_t *>(i_pAllocator->_alloc(m_numBytes));
 
@@ -16,19 +18,28 @@ BitArray::BitArray(size_t i_numBits, HeapManager * i_pAllocator, bool i_startCle
 
 BitArray::~BitArray()
 {
+	// TODO veify
+	m_allocatedUsed->_free(m_pBits);
 }
 
-void BitArray::ClearAll(void)
-{
-}
-
+// TODO put in inl.h file
 void BitArray::SetAll(void)
 {
+	size_t numElements = m_numBytes / sizeof(m_pBits);
+	for (unsigned int i = 0; i < numElements; i++)
+	{
+		m_pBits[i] = FULL_8_BYTES;
+	}
 }
 
-bool BitArray::AreAllClear(void) const
+// TODO put in inl.h file
+void BitArray::ClearAll(void)
 {
-	return false;
+	size_t numElements = m_numBytes / sizeof(m_pBits);
+	for (unsigned int i = 0; i < numElements; i++)
+	{
+		m_pBits[i] = 0x0;
+	}
 }
 
 bool BitArray::AreAllSet(void) const
@@ -36,34 +47,112 @@ bool BitArray::AreAllSet(void) const
 	return false;
 }
 
+bool BitArray::AreAllClear(void) const
+{
+	return false;
+}
+
+// TODO put in inl.h file
 inline bool BitArray::IsBitSet(size_t i_bitNumber) const
 {
-	return false;
+	size_t byteIndex = GetByteIndex(i_bitNumber);
+	size_t bitPosition = GetBitPosition(i_bitNumber);
+
+	uint8_t bitEvaluator = GetBitEvaluator(bitPosition);
+
+	return m_pBits[byteIndex] & bitEvaluator;
 }
 
+// TODO put in inl.h file
 inline bool BitArray::IsBitClear(size_t i_bitNumber) const
 {
-	return false;
+	return !IsBitSet(i_bitNumber);
 }
 
+// TODO put in inl.h file
 void BitArray::SetBit(size_t i_bitNumber)
 {
+	size_t byteIndex = GetByteIndex(i_bitNumber);
+	size_t bitPosition = GetBitPosition(i_bitNumber);
+
+	uint8_t bitEvaluator = GetBitEvaluator(bitPosition);
+
+	// using OR will set the bit we are concerned with
+	m_pBits[byteIndex] | bitEvaluator;
 }
 
+// TODO put in inl.h file
 void BitArray::ClearBit(size_t i_bitNumber)
 {
-}
+	size_t byteIndex = GetByteIndex(i_bitNumber);
+	size_t bitPosition = GetBitPosition(i_bitNumber);
 
-bool BitArray::GetFirstClearBit(size_t & o_bitNumber) const
-{
-	// TODO store last bit set and check next one first
+	uint8_t bitEvaluator = GetBitEvaluator(bitPosition);
 
-	return false;
+	// using XOR with inverse of both will clear bit we are concerned with
+	m_pBits[byteIndex] = ~m_pBits[byteIndex] ^ ~bitEvaluator;
 }
 
 bool BitArray::GetFirstSetBit(size_t & o_bitNumber) const
 {
-	return false;
+	size_t index = 0;
+
+	size_t numElements = m_numBytes / sizeof(m_pBits);
+
+	// skip bytes where bits are clear
+	while ((m_pBits[index] == 0x0) && (index < numElements))
+		index++;
+
+	if (index >= numElements)
+		return false;
+
+	// at the index there is a clear bit
+	uint64_t setBits = m_pBits[index];
+	size_t bit;
+
+	size_t numBitsInElement = sizeof(m_pBits) * NUM_BITS_IN_BYTE;
+	for (bit = 0; bit < numBitsInElement; bit++)
+	{
+		if (setBits & (1 << bit))
+			break;
+	}
+
+	// set out bit number based pf the element and bit
+	size_t o_bitNumber = index * numBitsInElement + bit;
+
+	// whether bit was found in for loop
+	return bit < numBitsInElement;
+}
+
+bool BitArray::GetFirstClearBit(size_t & o_bitNumber) const
+{
+	size_t index = 0;
+
+	size_t numElements = m_numBytes / sizeof(m_pBits);
+
+	// skip bytes where bits are set
+	while ((m_pBits[index] == FULL_8_BYTES) && (index < numElements))
+		index++;
+
+	if (index >= numElements)
+		return false;
+
+	// at the index there is a clear bit
+	uint64_t clearBits = m_pBits[index];
+	size_t bit;
+
+	size_t numBitsInElement = sizeof(m_pBits) * NUM_BITS_IN_BYTE;
+	for (bit = 0; bit < numBitsInElement; bit++)
+	{
+		if (!(clearBits & (1 << bit)))
+			break;
+	}
+
+	// set out bit number based pf the element and bit
+	size_t o_bitNumber = index * numBitsInElement + bit;
+
+	// whether bit was found in for loop
+	return bit < numBitsInElement;
 }
 
 bool BitArray::operator[](size_t i_index) const
--- FixedSizeAllocator/FixedSizeAllocator/BitArray.h
+++ FixedSizeAllocator/FixedSizeAllocator/BitArray.h
@@ -4,17 +4,38 @@
 
 #include <cstdint>
 
+#pragma region Global Constants
+
+const uint8_t NUM_BITS_IN_BYTE = 8;
+
+const uint64_t FULL_8_BYTES = 0xffffffffffffffff;
+
+#pragma endregion
+
+#pragma region Bit and Byte Helpers
+
+// the byte that has the bit number
+inline size_t GetByteIndex(size_t i_bitNumber) { return i_bitNumber / NUM_BITS_IN_BYTE; }
+
+// the bit position of the relevant byte
+inline size_t GetBitPosition(size_t i_bitNumber) { return i_bitNumber % NUM_BITS_IN_BYTE; }
+
+// returns a byte with the only the bit position set 1
+inline uint8_t GetBitEvaluator(size_t i_bitPosition) { return 1 << i_bitPosition; }
+
+#pragma endregion
+
 class BitArray
 {
 public:
 	BitArray(size_t i_numBits, HeapManager * i_pAllocator, bool i_startClear = true);
 	~BitArray();
 
-	void ClearAll(void);
 	void SetAll(void);
+	void ClearAll(void);
 
-	bool AreAllClear(void) const;
 	bool AreAllSet(void) const;
+	bool AreAllClear(void) const;
 
 	inline bool IsBitSet(size_t i_bitNumber) const;
 	inline bool IsBitClear(size_t i_bitNumber) const;
@@ -22,13 +43,14 @@ public:
 	void SetBit(size_t i_bitNumber);
 	void ClearBit(size_t i_bitNumber);
 
-	bool GetFirstClearBit(size_t & o_bitNumber) const;
 	bool GetFirstSetBit(size_t & o_bitNumber) const;
+	bool GetFirstClearBit(size_t & o_bitNumber) const;
 
 	bool operator[](size_t i_index) const;
 
 private:
 	uint64_t * m_pBits;
+	HeapManager * m_allocatedUsed;
 	const size_t m_numBits;
 	const size_t m_numBytes;
 };
