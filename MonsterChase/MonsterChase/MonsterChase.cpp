e47178f0f750fc760c5fc34798f85c0063122ab6
ewaugh13 <ewaugh13@yahoo.com>
2020-01-15 12:58:37 -0700

    Basic rendering of white sqaure now. Seems like all GLib stuff is configured.

--- MonsterChase/MonsterChase/MonsterChase.cpp
+++ MonsterChase/MonsterChase/MonsterChase.cpp
@@ -1,101 +1,83 @@
+#include "Allocators.h"
+#include "Engine.h"
+#include "GamePlay.h"
+#include "GLib.h"
+#include "MonsterMovement.h"
+#include "PlayerMovement.h"
+
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <crtdbg.h>
 #include <malloc.h>
-#include <PlayerMovement.h>
-#include <MonsterMovement.h>
-
-#include "Engine.h"
-#include "GamePlay.h"
-#include "Allocators.h"
+#include <Windows.h>
 
 extern const int GRID_SIZE = 100;
 extern bool PLAYING = true;
 // number of turns that go by before we generate a new monster
 const int TURN_AMOUNT_GENERATE_MONSTER = 9;
 
-int main()
-{
-	{
-		Engine * engine = new Engine();
-		GamePlay * gameplay = new GamePlay(TURN_AMOUNT_GENERATE_MONSTER);
-
-		// get the player name and set a location
-		std::cout << "What would you like to name the Player" << ": ";
-		char * playerName;
-		engine->getPlayerInput(playerName);
-
-		Actor * player = new Actor(playerName, Point2D::Zero);
-		engine->freeMemory(playerName);
-
-		// add player components
-		player->AddComponent(new PlayerMovement());
-
-		// get the number of monsters to create
-		std::cout << "How many monsters do you want to start with: ";
-		char * numMonstersInput;
-		engine->getPlayerInput(numMonstersInput);
-		unsigned int numMonsters = atoi(numMonstersInput);
-
-		Actor ** monsters = (Actor **)engine->initalizeMemory(numMonsters, sizeof(Actor *));
-
-		// get each monster name and set a location
-		for (unsigned int i = 0; i < numMonsters; i++)
-		{
-			std::cout << "What would you like to name Monster " << i << ": ";
-			char * monsterName;
-			engine->getPlayerInput(monsterName);
-
-			// create monster and add component for movement
-			monsters[i] = new Actor(monsterName, Point2D::GetRandomPosition(GRID_SIZE));
-			monsters[i]->AddComponent(new MonsterMovement(player));
-			engine->freeMemory(monsterName);
-		}
-
-		// while loop for game loop until user input to quit
-		gameplay->gamePlayLoop(engine, player, monsters, numMonsters);
-
-		// free everything we allocated
-		engine->freeMemory(numMonstersInput);
-		for (unsigned int i = 0; i < numMonsters; i++)
-		{
-			delete monsters[i];
-		}
-		engine->freeMemory(monsters);
-
-		delete player;
-		delete engine;
-		delete gameplay;
-	}
-	_CrtDumpMemoryLeaks();
-	return 0;
-}
-
-// call this from main if you want to test it
-// commented out since it makes cpp check have a bunch of warnings
-
-//void Point2D_UnitTest()
+//int main()
 //{
-//	Point2D P1(0.0f, 0.0f);
-//	Point2D P2(0.0f, 0.0f);
+//	{
+//		Engine * engine = new Engine();
+//		GamePlay * gameplay = new GamePlay(TURN_AMOUNT_GENERATE_MONSTER);
 //
-//	Point2D P3 = P1 + P2;
-//	P3 = P1 - P2;
+//		// get the player name and set a location
+//		std::cout << "What would you like to name the Player" << ": ";
+//		char * playerName;
+//		engine->getPlayerInput(playerName);
 //
-//	Point2D P4 = P1 * 0.5f;
+//		Actor * player = new Actor(playerName, Point2D::Zero);
+//		engine->freeMemory(playerName);
 //
-//	Point2D P5 = P1 / 2.0f;
+//		// add player components
+//		player->AddComponent(new PlayerMovement());
 //
-//	Point2D P6 = -P4;
+//		// get the number of monsters to create
+//		std::cout << "How many monsters do you want to start with: ";
+//		char * numMonstersInput;
+//		engine->getPlayerInput(numMonstersInput);
+//		unsigned int numMonsters = atoi(numMonstersInput);
 //
-//	P6 *= 2.0f;
-//	P5 /= 4.0f;
+//		Actor ** monsters = (Actor **)engine->initalizeMemory(numMonsters, sizeof(Actor *));
 //
-//	P2 += P1;
-//	P3 -= P1;
+//		// get each monster name and set a location
+//		for (unsigned int i = 0; i < numMonsters; i++)
+//		{
+//			std::cout << "What would you like to name Monster " << i << ": ";
+//			char * monsterName;
+//			engine->getPlayerInput(monsterName);
 //
-//	Point2D P7 = (((P1 + P2) * 2.0f) - -P3) / 2.0f;
+//			// create monster and add component for movement
+//			monsters[i] = new Actor(monsterName, Point2D::GetRandomPosition(GRID_SIZE));
+//			monsters[i]->AddComponent(new MonsterMovement(player));
+//			engine->freeMemory(monsterName);
+//		}
 //
-//	bool bArentEqual = P6 != P4;
+//		// while loop for game loop until user input to quit
+//		gameplay->gamePlayLoop(engine, player, monsters, numMonsters);
+//
+//		// free everything we allocated
+//		engine->freeMemory(numMonstersInput);
+//		for (unsigned int i = 0; i < numMonsters; i++)
+//		{
+//			delete monsters[i];
+//		}
+//		engine->freeMemory(monsters);
+//
+//		delete player;
+//		delete engine;
+//		delete gameplay;
+//	}
+//	_CrtDumpMemoryLeaks();
+//	return 0;
 //}
+
+int WINAPI wWinMain(HINSTANCE i_hInstance, HINSTANCE i_hPrevInstance, LPWSTR i_lpCmdLine, int i_nCmdShow)
+{
+	// IMPORTANT: first we need to initialize GLib
+	bool bSuccess = GLib::Initialize(i_hInstance, i_nCmdShow, "GLibTest", -1, 800, 600);
+
+	while (true);
+}
