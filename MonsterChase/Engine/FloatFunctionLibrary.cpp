0b20726a9b61c67fa483a7bce2a066aeebd06046
ewaugh13 <ewaugh13@yahoo.com>
2020-02-02 00:56:47 -0700

    Created float library functions. Need to create unit test and convert any float usages if needed.

---
+++ MonsterChase/Engine/FloatFunctionLibrary.cpp
@@ -0,0 +1,59 @@
+#include "FloatFunctionLibrary.h"
+
+inline bool FloatFunctionLibrary::AlmostEqualRelativeAndAbs(float i_A, float i_B, float i_maxDiff, float i_maxRelDiff)
+{
+	// Check if the numbers are really close -- needed
+	// when comparing numbers near zero.
+	float diff = static_cast<float>(fabs(i_A - i_B));
+	if (diff <= i_maxDiff)
+		return true;
+
+	i_A = static_cast<float>(fabs(i_A));
+	i_B = static_cast<float>(fabs(i_B));
+	// Find the largest
+	float largest = (i_B > i_A) ? i_B : i_A;
+
+	if (diff <= largest * i_maxRelDiff)
+		return true;
+	return false;
+}
+
+bool FloatFunctionLibrary::AlmostEqualUlpsAndAbs(float i_A, float i_B, float i_maxDiff, unsigned int i_maxUlpsDiff)
+{
+	// Check if the numbers are really close -- needed
+	// when comparing numbers near zero.
+	float absDiff = static_cast<float>(fabs(i_A - i_B));
+	if (absDiff <= i_maxDiff)
+		return true;
+
+	Float_t uA(i_A);
+	Float_t uB(i_B);
+
+	// Different signs means they do not match.
+	if (uA.Negative() != uB.Negative())
+	{
+		// Check for equality to make sure +0==-0
+		if (i_A == i_B)
+			return true;
+		return false;
+	}
+
+	// Find the difference in ULPs.
+	unsigned int ulpsDiff = abs(uA.i - uB.i);
+	if (ulpsDiff <= i_maxUlpsDiff)
+		return true;
+
+	return false;
+}
+
+inline bool FloatFunctionLibrary::AlmostEqualZeroCertain(float i_A, float i_maxCertDiff)
+{
+	return fabs(i_A) <= i_maxCertDiff;
+}
+
+inline bool FloatFunctionLibrary::IsNan(float i_A)
+{
+	volatile float volA = i_A;
+	// If not equal to itself it's a NaN (according to IEEE standard)
+	return volA != volA;
+}
---
+++ MonsterChase/Engine/FloatFunctionLibrary.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "float.h"		/* FLT_EPSILON */
+#include <math.h>		/* fabs */
+#include <stdint.h>		/* int32_t, uint32_t */
+
+/* See https://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/ for reference on these functions*/
+
+union Float_t
+{
+	Float_t(float num = 0.0f) : f(num) {}
+	// Portable extraction of components.
+	bool Negative() const { return i < 0; }
+	int32_t RawMantissa() const { return i & ((1 << 23) - 1); }
+	int32_t RawExponent() const { return (i >> 23) & 0xFF; }
+
+	int32_t i;
+	float f;
+#ifdef _DEBUG
+	struct
+	{   // Bitfields for exploration. Do not use in production code.
+		uint32_t mantissa : 23;
+		uint32_t exponent : 8;
+		uint32_t sign : 1;
+	} parts;
+#endif
+};
+
+namespace FloatFunctionLibrary
+{
+	// Equal within a relative epsilon
+	bool AlmostEqualRelativeAndAbs(float i_A, float i_B, float i_maxDiff, float i_maxRelDiff = FLT_EPSILON);
+
+	// Equal within a certain Ulps
+	bool AlmostEqualUlpsAndAbs(float i_A, float i_B, float i_maxDiff, unsigned int i_maxUlpsDiff);
+
+	// Equal to zero within a certain epsilon
+	bool AlmostEqualZeroCertain(float i_A, float i_maxCertDiff = FLT_EPSILON);
+
+	// If value is a NaN
+	bool IsNan(float i_A);
+};
+
