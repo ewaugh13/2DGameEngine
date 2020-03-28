f407b1b66aaa3898b7ddd6fd90c79940f2d69950
ewaugh13 <ewaugh13@yahoo.com>
2020-03-28 01:15:25 -0600

    Fleshed out more of SmartPtr functionality and unit testing. Based off of std::shared_ptr intereface and comparision operators plus move semantics constructor and operator equals.

--- MonsterChase/Engine/SmartPtr.h
+++ MonsterChase/Engine/SmartPtr.h
@@ -6,17 +6,34 @@ template<class T >
 class SmartPtr
 {
 public:
+	// Empty constructor
+	SmartPtr() :
+		m_Ptr(nullptr), m_ReferenceCount(nullptr)
+	{
+	}
+
+	// Constructor with ptr
 	explicit SmartPtr(T * i_Ptr) :
 		m_Ptr(i_Ptr), m_ReferenceCount(new ReferenceCount(1, 0))
 	{
 	}
 
+	// Copy Constructor
 	SmartPtr(SmartPtr & i_Other) :
 		m_Ptr(i_Other.m_Ptr), m_ReferenceCount(i_Other.m_ReferenceCount)
 	{
-		(*m_ReferenceCount->m_SmartPtrsCount)++;
+		m_ReferenceCount->m_SmartPtrsCount++;
 	}
 
+	// Move copy constructor
+	SmartPtr(SmartPtr && i_Other) noexcept :
+		m_Ptr(i_Other.m_Ptr), m_ReferenceCount(i_Other.m_ReferenceCount)
+	{
+		i_Other.m_Ptr = nullptr;
+		i_Other.m_ReferenceCount = nullptr;
+	}
+
+	// Assignment operator
 	SmartPtr & operator=(SmartPtr & i_Other)
 	{
 		if (this != &i_Other)
@@ -32,6 +49,30 @@ public:
 		return *this;
 	}
 
+	// Move Assignment operator
+	SmartPtr& operator=(SmartPtr && i_Other)
+	{
+		if (m_Ptr != i_Other.m_Ptr)
+		{
+			Release();
+
+			m_Ptr = i_Other.m_Ptr;
+			m_ReferenceCount = i_Other.m_ReferenceCount;
+
+			i_Other.m_Ptr = nullptr;
+			i_Other.m_ReferenceCount = nullptr;
+		}
+
+		return *this;
+	}
+
+	// Destructor
+	~SmartPtr()
+	{
+		Release();
+	}
+
+	// Assignment operator to nullptr
 	SmartPtr & operator=(std::nullptr_t i_Nullptr)
 	{
 		Release();
@@ -42,39 +83,91 @@ public:
 		return *this;
 	}
 
-	/*inline*/ bool operator==(std::nullptr_t i_Nullptr) const
+	// If two smart ptrs are holding the same ptr
+	template<class E>
+	inline bool operator==(const SmartPtr<E> & i_Other) const
 	{
-		return pPtr != nullptr;
+		return m_Ptr == i_Other.m_Ptr;
 	}
 
-	~SmartPtr()
+	// If the smart ptr is holding this value
+	template<class E>
+	inline bool operator==(const E * i_Other) const
 	{
-		Release();
+		return m_Ptr == i_Other;
 	}
 
+	// If two smart ptrs are holding a different ptr
+	template<class E>
+	inline bool operator!=(const SmartPtr<E> & i_Other) const
+	{
+		return m_Ptr != i_Other.m_Ptr;
+	}
+
+	// If the smart ptr is holding this value
+	template<class E>
+	inline bool operator!=(const E * i_Other) const
+	{
+		return m_Ptr != i_Other;
+	}
+
+	// If a smart ptr holds a nullptr and is null
+	inline bool operator==(std::nullptr_t i_Nullptr) const
+	{
+		return m_Ptr == nullptr;
+	}
+
+	// Return the ptr this smart ptr holds
 	T * operator->()
 	{
 		return m_Ptr;
 	}
 
+	// Return the value this smart ptr holds
 	T & operator*()
 	{
 		return (*m_Ptr);
 	}
 
-	/*inline*/ operator bool() const
+	// If the smart ptr is valid
+	inline operator bool() const
 	{
 		return m_Ptr != nullptr;
 	}
 
+	// Index into smart ptr holding array ptr
+	inline T operator[](int i_Index)
+	{
+		return m_Ptr[i_Index];
+	}
+
+	// Return num or smart ptrs reference count
+	inline unsigned int UseCount()
+	{
+		return m_ReferenceCount->m_SmartPtrsCount;
+	}
+
+	// Swap two different smart ptrs ptrs they are holding
+	inline void Swap(SmartPtr & i_Other)
+	{
+		T * temp = i_Other.m_Ptr;
+		i_Other.m_Ptr = m_Ptr;
+		m_Ptr = temp;
+	}
+
 private:
 
+	// Decrement smart ptr count and delete object if no more references
 	void Release()
 	{
 		if (m_ReferenceCount != nullptr && --m_ReferenceCount->m_SmartPtrsCount == 0)
 		{
 			delete m_Ptr;
+			// TODO: check for WeakPtr once it is implemented
 			delete m_ReferenceCount;
+
+			m_Ptr = nullptr;
+			m_ReferenceCount = nullptr;
 		}
 	}
 
--- MonsterChase/UnitTests/SmartWeakPtrTest.cpp
+++ MonsterChase/UnitTests/SmartWeakPtrTest.cpp
@@ -4,13 +4,13 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace WeakPtrTest
+namespace SmartWeakPtrTest
 {
-	TEST_CLASS(Point2DUnitTest1)
+	TEST_CLASS(SmartWeakPtrUnitTest1)
 	{
 	public:
 
-		TEST_METHOD(JoesTest)
+		TEST_METHOD(SmartPtrTest)
 		{
 			SmartPtr<Point2D> point2DSmartPtr = SmartPtr<Point2D>(new Point2D(5.0f, 5.0f));
 			SmartPtr<Point2D> point2DZeroSmartPtr = SmartPtr<Point2D>(new Point2D(0.0f, 0.0f));
@@ -28,7 +28,26 @@ namespace WeakPtrTest
 			point2DSmartPtr = SmartPtr<Point2D>(new Point2D(6.0f, 6.0f));
 			point2DSmartPtr = point2DZeroSmartPtr;
 
-			point2DZeroSmartPtr.~SmartPtr();
+			point2DSmartPtr = nullptr;
+			Assert::IsTrue(point2DSmartPtr == nullptr);
+
+			SmartPtr<int> intSmartPtr = SmartPtr<int>(new int[2]{});
+			int * pInt = intSmartPtr.operator->();
+			pInt[0] = 0;
+			pInt[1] = 5;
+			Assert::AreEqual(5, intSmartPtr[1]);
+
+			SmartPtr<int> intSmartPtrCopy = intSmartPtr;
+			unsigned int expectedUseCount = 2;
+			Assert::AreEqual(expectedUseCount, intSmartPtrCopy.UseCount());
+
+			SmartPtr<int> intSmartPtrDiff = SmartPtr<int>(new int[2]{});
+			int * pIntDiff = intSmartPtrDiff.operator->();
+			pIntDiff[0] = 100;
+			pIntDiff[1] = 500;
+
+			intSmartPtr.Swap(intSmartPtrDiff);
+			Assert::AreEqual(500, intSmartPtr[1]);
 		}
 	};
 }
